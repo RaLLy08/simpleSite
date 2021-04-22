a23d212998a3c3c36779f050b326793932daf34f
azadk@forit.az <uytemataaero@mail.ru>
2021-04-23 01:48:36 +0400

    alg names

--- src/@simple/components/AnimateText.tsx
+++ src/@simple/components/AnimateText.tsx
@@ -1,13 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import { Button } from '@material-ui/core';
 import { useRef } from 'react';
+import { makeStyles } from '@material-ui/styles';
+import RefreshIcon from '@material-ui/icons/Refresh';
 
 import { AnimateTextInterface, SortingTypesType, WordForSortType, SortingAlgoritmsType } from './types';
 import { bubbleSort, selectionSort } from '../genSortingAlgoritms';
 import { SortingGenerator } from '../genSortingAlgoritms/types';
+import clsx from 'clsx';
+
+const useStyles = makeStyles(theme => ({
+    textBox: {
+        wordBreak: "break-all"
+    },
+}))
 
 
 const AnimateText = (props: AnimateTextInterface) => {
+    const classes = useStyles();
+
     const { text, speed = 100, type } = props;
     // by words speed / 10 or word math
     const sortedText: Array<WordForSortType> = Array.from(text).map((el, i) => ({ value: el, index: i }));
@@ -16,26 +27,42 @@ const AnimateText = (props: AnimateTextInterface) => {
     const [animText, setAnimText] = useState<Array<WordForSortType>>(mixedText);
     const [isDone, setDone] = useState<boolean>(true);
     const timeoutRef = useRef<any>();
+    const algNameRef = useRef<string>();
 
     const getSortingAlgoritms = (mixedText: Array<WordForSortType>): SortingAlgoritmsType => ({
         selection: selectionSort(mixedText),
         bubble:    bubbleSort(mixedText),
     });
 
+    const onRefresh = () => {
+        if (!isDone) {
+            clearTimeout(timeoutRef.current);
+            setDone(true);
+        };
+
+        init();
+    }
+
     const getSortingAlgoritm =  (mixedText: Array<WordForSortType>, type: SortingTypesType): SortingGenerator => {
         const algoritms = getSortingAlgoritms(mixedText);
  
         if (type === 'random') {
             const algsArr = Object.values(algoritms);
-            
-            return algsArr[Math.round(Math.random() * (algsArr.length - 1))]; // random from array
+            const algsNamesArr = Object.keys(algoritms);
+
+            const randInx: number = Math.round(Math.random() * (algsArr.length - 1));
+            algNameRef.current = algsNamesArr[randInx];
+
+            return algsArr[randInx]; // random from array
         }
+        algNameRef.current = type;
 
         return algoritms[type];
     };
 
     const init = () => {
         const gen = getSortingAlgoritm(mixedText, type);
+        // algNameRef
         setDone(false);
 
         const update = () => {
@@ -58,12 +85,16 @@ const AnimateText = (props: AnimateTextInterface) => {
 
         return () => clearTimeout(timeoutRef.current);
     }, [])
-    
-    return <div style={{wordBreak: "break-all"}}>
-        {animText.map((el: WordForSortType) => el.value)}
-        <Button disabled={!isDone} onClick={() => { init()}}>
-            Refresh
-        </Button>
+    // clsx(!isDone && classes.textBox, 'flex')
+    return <div className="flex flex-col">
+        <div className={clsx(classes.textBox, 'flex')}>
+            {animText.map((el: WordForSortType) => el.value)}
+        </div>
+        <div className="flex justify-end">
+            <Button size="small" onClick={() => { onRefresh()}}>
+                {!isDone ? algNameRef.current : <RefreshIcon/>} 
+            </Button>
+        </div>
     </div>;
 }
 
